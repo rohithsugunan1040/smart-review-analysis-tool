872a915ff6e83965f88fbf12168a6df2edaeb4aa
sibi <sibikottukandiyil2@gmail.com>
2025-01-29 18:27:47 +0530

    Keyword analysis added

--- src/charts.py
+++ src/charts.py
@@ -1,5 +1,34 @@
 import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import nltk
+from rake_nltk import Rake
+import os
+
+from collections import Counter
+
+try:
+    nltk.data.find('corpora/stopwords')
+    print("Stopword resource already available")
+except LookupError:
+    print("Downloading stopword resource")
+    nltk.download('stopwords')
+
+
+# Find NLTK data paths
+nltk_data_paths = nltk.data.path
+
+# Check each directory for 'punkt_tab'
+for path in nltk_data_paths:
+    punkt_path = os.path.join(path, 'tokenizers', 'punkt')
+    if os.path.exists(punkt_path):
+        files = os.listdir(punkt_path)
+        if 'punkt_tab.py' in files:
+            print(f"'punkt_tab.py' found in: {punkt_path}")
+        else:
+            print(f"'punkt_tab.py' not found in: {punkt_path}")
+            nltk.download('punkt_tab')
 
 def create_pie_chart(sentiments):
     labels = sentiments.keys()
@@ -18,8 +47,39 @@ def create_pie_chart(sentiments):
         autotext.set_color('white')
         autotext.set_fontsize(10)
     
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn=' as a circle.
     plt.setp(autotexts, size=10, weight="bold")
     ax1.set_title('Sentiment Analysis Results', color='black', fontsize=14)
     
     st.pyplot(fig1)
+
+
+def create_bar_chart(cleaned_reviews):
+    r = Rake(min_length=2, max_length=3)
+    all_keywords = []
+    for review in cleaned_reviews:
+        r.extract_keywords_from_text(review)
+        all_keywords.extend(r.get_ranked_phrases())
+    
+    keyword_counts = Counter(all_keywords)
+
+
+    keywords = []
+    counts = []
+    for keyword,count in keyword_counts.items():
+        if count > 4:
+            keywords.append(keyword)
+            counts.append(count)
+
+
+    df = pd.DataFrame({
+        'keyword' : keywords,
+        'count' : counts
+    })
+
+    fig = px.bar(df,x='keyword',y='count',labels={
+        'keyword':'keywords','count':'keyword count'
+    },title='Most occured Keywords')
+
+    st.plotly_chart(fig)
+
--- src/review_scraper.py
+++ src/review_scraper.py
@@ -9,7 +9,15 @@ from selenium.webdriver import ActionChains
 
 from selenium.common.exceptions import NoSuchElementException
 
-import time
+import sys
+import os
+import streamlit as st
+
+driver_path = ChromeDriverManager().install()
+if os.path.exists(driver_path):
+    print("ChromeDriver already installed at:", driver_path)
+else:
+    print("Installing ChromeDriver...")
 
 
 #FOR OPTIONS
@@ -18,23 +26,25 @@ op.add_argument('headless')
 
 
 # Setup ChromeDriver using WebDriver Manager
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service,options=op)
-
+service = Service(driver_path)
 
 def fetch_reviews(link):
+    driver = webdriver.Chrome(service=service)
     driver.get(link)
 
     while driver.execute_script("return document.readyState") != "complete":
         pass
 
     print('\nPAGE FULLY LOADED!!\n')
-
-    all_review_button = driver.find_element(By.CSS_SELECTOR,"._23J90q.RcXBOT")
-
-
+    try:
+        all_review_button = driver.find_element(By.CSS_SELECTOR,"._23J90q.RcXBOT")
+    except NoSuchElementException:
+        try:
+            all_review_button = driver.find_element(By.CSS_SELECTOR,"._23J90q.iIbIvC")
+        except NoSuchElementException:
+            st.error("Review not found")
+            return 0
     anchor = all_review_button.find_element(By.XPATH,'..')
-
     anchor_link = anchor.get_attribute("href")
 
     reviews = []
@@ -44,7 +54,9 @@ def fetch_reviews(link):
         while(driver.find_element(By.CLASS_NAME,"_9QVEpD")):
             while driver.execute_script("return document.readyState") != "complete":
                 pass
-            print(f'\nPAGE {page_count} LOADED\n')
+
+            sys.stdout.write(f"\rPAGE {page_count} LOADED")
+            sys.stdout.flush()
 
             review_divs = driver.find_elements(By.CLASS_NAME,"ZmyHeo")
 
@@ -57,36 +69,6 @@ def fetch_reviews(link):
             page_count += 1
             driver.get(anchor_link+"&page="+str(page_count))
     except NoSuchElementException:
-        print("ELEMENT NOT FOUND")
+        print("\nEND OF PAGINATION")
+    driver.quit()
     return reviews
-    
-
-
-
-
-
-
-
-
-# anchor.click()
-
-# while driver.execute_script("return document.readyState") != "complete":
-#     pass
-# print("\nALL REVIEW PAGE LOADED!!\n")
-# time.sleep(3)
-# print("CURRENT URL")
-# print(driver.current_url)
-
-# for i in range(1,4):
-#     review_divs
-
-
-# review_divs = driver.find_elements(By.CLASS_NAME,"ZmyHeo")
-
-
-# for div in review_divs:
-#     read_more = div.find_element(By.TAG_NAME,"span")
-#     if read_more.is_displayed():
-#         read_more.click()
-#     print(div.find_element(By.XPATH,'.//div').text)
-#     print("------------------------------------------")
